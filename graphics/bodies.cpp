822f1c3038a3e3fa8d6a83f19d437c894d076990
MattBJ <40750771+MattBJ@users.noreply.github.com>
2021-02-13 15:46:58 -0800

    Add files via upload

---
+++ graphics/bodies.cpp
@@ -0,0 +1,303 @@
+#include "bodies.hpp"
+
+constexpr uint16_t PPM = 4; // 16 pixels per meter
+constexpr float dt = (1.0/60); // 60 fps
+constexpr float MPS = 1.0; // 1 meter / second == x / dt.. just use dt
+
+constexpr double pi = 3.141592653589793;
+
+void Pixel::newPixel(uint16_t _x, uint16_t _y){
+	x.push_back(_x);
+	y.push_back(_y);
+	return;
+}
+
+void Pixel::clear(){
+	x.clear();
+	y.clear();
+}
+
+Pixel Pixel::copy(){
+	Pixel out;
+	out.x = x;
+	out.y = y;
+	return out;
+}
+
+Ball::Ball(float _radius, float _xCenter, float _yCenter) : 
+pos(_xCenter,_yCenter), vel((MPS*10)*dt,(MPS*5)*dt), acc((MPS/2)*dt,(MPS/2)*dt) // just for now
+{
+	radius = _radius;
+	theta = 0;
+	thetaDot = 0;
+	theta2Dot = 0;
+
+	// update() initializes:
+		// min's and max's, as well as the corners
+		// painter class
+	update();
+	return;
+}
+
+void Ball::update(){
+	painter.clear();
+	min_x = static_cast<int32_t>((pos(coordinate::X) - radius)*PPM + 0.5);
+	min_y = static_cast<int32_t>((pos(coordinate::Y) - radius)*PPM + 0.5);
+	max_x = static_cast<int32_t>((pos(coordinate::X) + radius)*PPM + 0.5);
+	max_y = static_cast<int32_t>((pos(coordinate::Y) + radius)*PPM + 0.5);
+	uint16_t i, j;
+	// iterate over square (bottom left: position x/y - radius.. converted to PPM)
+	// adding 0.5 for rounding, it gets floored
+	uint16_t quantizedR = static_cast<uint16_t>(radius * PPM + 0.5);
+	std::cout << "Center pixel: (" 	<< static_cast<uint16_t>(pos(coordinate::X)*PPM+0.5) << ","
+									<< static_cast<uint16_t>(pos(coordinate::Y)*PPM + 0.5) << ")\n";
+	
+	std::cout <<
+	"\tmin_x:\t" << min_x << std::endl << 
+	"\tmax_x:\t" << max_x << std::endl << 
+	"\tmin_y:\t" << min_y << std::endl << 
+	"\tmax_y:\t" << max_y << std::endl <<
+	"\tvelX:\t" << vel(coordinate::X) << std::endl <<
+	"\tvelY:\t" << vel(coordinate::Y) << std::endl;
+
+	// @TODO: add clipping range on max side as well --> WINDOW_WIDTH/HEIGHT
+	for(i= (min_x>=0)? min_x : 0;i<max_x;i++){ // x axis pixels
+		for(j=(min_y>=0)? min_y : 0;j<max_y;j++){ // y axis pixels
+			// now figure out distance from center
+			Vector2f checkPoint(static_cast<float>(i)/PPM,static_cast<float>(j)/PPM);
+			uint16_t distPix = static_cast<uint16_t>(
+						((checkPoint-pos).norm()*PPM)+0.5 // difference vector is in meters, convert, add 0.5 for floor rounding
+						);
+			if( distPix <= quantizedR){
+				painter.newPixel(i,j);
+			}
+		}
+	}
+	return;
+}
+
+// void Ball::move(Vector2f jerk){ // change in acceleration
+// 	acc = acc + jerk;
+// 	move();
+// }
+
+ // constant acceleration
+
+void Ball::move(bool angular){ // default = false
+	// for now let's always use angular
+	if(angular){
+		Matrix3f velAf;
+		// cos,sin
+		// -sin,cos --> counterclockwise orientation
+		velAf <<	cos(thetaDot),		sin(thetaDot),	vel(coordinate::X),
+					-sin(thetaDot),		cos(thetaDot),	vel(coordinate::Y),
+					0,					0,				1;
+		Matrix3f accAf;
+		accAf <<	cos(theta2Dot),		sin(theta2Dot),	acc(coordinate::X),
+					-sin(theta2Dot),	cos(theta2Dot),	acc(coordinate::Y),
+					0,					0,				1;
+		Vector3f tmpPos;
+		tmpPos << pos, 1; // concatenates a 1 on the end of position vector and gets assigned to tmpPos
+		Vector3f tmpVel;
+		tmpVel << vel, 1;
+		// Vector3f tmpAcc(acc(coordinate::X),acc(coordinate::Y),1);
+
+		tmpPos = velAf * tmpPos;
+		tmpVel = accAf * tmpVel;
+		pos(coordinate::X) = tmpPos(coordinate::X);
+		pos(coordinate::Y) = tmpPos(coordinate::Y);
+
+		vel(coordinate::X) = tmpVel(coordinate::X);
+		vel(coordinate::Y) = tmpVel(coordinate::Y);
+
+		// acc(coordinate::X) = tmpAcc(coordinate::X);
+		// acc(coordinate::Y) = tmpAcc(coordinate::Y);
+		theta += thetaDot;
+		thetaDot += theta2Dot;
+		return;
+	} else{
+		vel += acc;
+		pos += vel;
+		return;
+	}
+}
+
+void Ball::bounce(uint16_t _width, uint16_t _height){ // max width/height of window
+	uint8_t bitField = 0;
+	// (max_x > _width) || (min_x < 0)
+	Matrix3f Affine;
+	if((bitField = 		(max_x > _width)? 1 : 
+						(min_x < 0)? 2 : 0)){
+		// 1 means reflect from right side
+		// 2 means reflect from y axis
+
+		// case 1: Changing reference frame from origin to right border axis
+			// shift reference frame for vector
+			// do Affine reflection PLUS the shift back --> back to original ref frame
+
+		Vector3f afVec;
+		afVec << pos, 1; //pos(0) aka pos(coordinate::X) is going to be replaced
+
+		// reference frame: the rvalue of a subtraction
+			// case 1: diff vec points from max border to the position vector
+		afVec(coordinate::X) = (bitField == 1)? pos(coordinate::X) - (static_cast<float>(_width)/PPM - radius): 
+												pos(coordinate::X) - (radius - 0);
+
+		Affine << 	-1,0,0, // need to edit this dX component
+					0,1,0,
+					0,0,1;
+		Affine(0,2) = (bitField == 1)? (static_cast<float>(_width)/PPM - radius) : (radius - 0);
+
+		afVec = Affine * afVec;
+		pos(coordinate::X) = afVec(coordinate::X);
+		pos(coordinate::Y) = afVec(coordinate::Y);
+		// reflection operation complete
+		// update velocity/acceleration after performing the proper reflection
+		vel(coordinate::X) *= -1;
+		acc(coordinate::X) *= -1;
+		// not doing any angular stuff here..
+		bitField = 0; // i think this is redundant
+		update(); // redraw the pixels
+	}
+	// (max_y > _height) || (min_y < 0)
+	if((bitField = 		(max_y > _height)? 1 : 
+						(min_y < 0)? 2 : 0)){
+		// 1 means reflect from top axis
+		// 2 means reflect from x axis
+
+		Vector3f afVec;
+		afVec << pos,1; //pos(1) aka pos(coordinate::Y) is going to be replaced
+
+		afVec(coordinate::Y) = (bitField == 1)? pos(coordinate::Y) - (static_cast<float>(_height)/PPM - radius) :
+												pos(coordinate::Y) - (radius - 0);
+
+		Affine <<	1,0,0,
+					0,-1,0, // need to edit this dY component
+					0,0,1;
+
+		Affine(1,2) = (bitField == 1)? (static_cast<float>(_height)/PPM - radius) : (radius - 0);
+		afVec = Affine * afVec;
+		pos(coordinate::X) = afVec(coordinate::X);
+		pos(coordinate::Y) = afVec(coordinate::Y);
+
+		vel(coordinate::Y) *= -1;
+		acc(coordinate::Y) *= -1;
+		bitField = 0;
+		update();
+	}
+	return;
+}
+
+Rectangle::Rectangle(float _length, float _width, float _posX, float _posY) :
+	pos(_posX,_posY), vel((MPS*10)*dt,(MPS*5)*dt), acc((MPS/10)*dt,(MPS/10)*dt) // arbitrary stuff
+	// rPos is going to be recalculated
+	// rPos(0,0), rVel(0,0), rAcc(0,0)
+{
+	theta = 0;
+	thetaDot = 0;
+	theta2Dot = 0;
+	length = _length;
+	width = _width;
+
+	update();
+	return;
+}
+
+// update painter
+void Rectangle::update(){
+	painter.clear();
+
+	// figure out the borders
+	// pos represents one corner
+	
+	// initialize the corner position
+	// corners built from pos vector reference frame
+	for(uint8_t i = 0; i < 4; i++){
+		corners[i] << pos;
+		if(i==0){ // just a copy
+			continue;
+		}
+		// first and last corners shifted up by length
+		// Do projection with the 2 vector magnitudes
+		if(not((i-1) % 2)){ // same as !
+			// projection in the Y hat direction
+			corners[i](coordinate::Y) += cos(theta)*length;
+		}
+		// last 2 corners shifted by width
+		if((i-1)){
+			// projection in X hat direction
+			corners[i](coordinate::X) += cos(theta)*width;
+		}
+	}
+
+	std::cout << "\ncorners[0]:\n" << corners[0].transpose() << std::endl;
+	std::cout << "\ncorners[1]:\n" << corners[1].transpose() << std::endl;
+	std::cout << "\ncorners[2]:\n" << corners[2].transpose() << std::endl;
+	std::cout << "\ncorners[3]:\n" << corners[3].transpose() << std::endl;
+
+	max_x = max_y = std::numeric_limits<int32_t>::min();
+	min_x = min_y = std::numeric_limits<int32_t>::max();
+
+	for(uint8_t i = 0; i < 4; i++){
+		min_x =	(static_cast<int32_t>(corners[i](coordinate::X)*PPM + 0.5) < min_x)? static_cast<int32_t>(corners[i](coordinate::X)*PPM + 0.5) : min_x;
+		max_x = (static_cast<int32_t>(corners[i](coordinate::X)*PPM + 0.5) > max_x)? static_cast<int32_t>(corners[i](coordinate::X)*PPM + 0.5) : max_x;
+		min_y = (static_cast<int32_t>(corners[i](coordinate::Y)*PPM + 0.5) < min_y)? static_cast<int32_t>(corners[i](coordinate::Y)*PPM + 0.5) : min_y;
+		max_y = (static_cast<int32_t>(corners[i](coordinate::Y)*PPM + 0.5) > max_y)? static_cast<int32_t>(corners[i](coordinate::Y)*PPM + 0.5) : max_y;
+	}
+	std::cout << "min_x:\t" << min_x << std::endl;
+	std::cout << "max_x:\t" << max_x << std::endl;
+	std::cout << "min_y:\t" << min_y << std::endl;
+	std::cout << "max_y:\t" << max_y << std::endl;
+
+	// special case or not
+	bool specialCase = false;
+	for(uint8_t i = 1; i < 4; i++){
+		// check corner[0] quantized with the rest
+		uint8_t inc = 0;
+		inc += (corners[0] == corners[i])? 1 : 0;
+		specialCase = (inc == 2)? true : false; 
+		if(specialCase){
+			break; // stop checking
+		}
+	}
+	// special case --> paint every loop!
+	Vector2f painterCorners[4];
+	if(!specialCase){ // need to put our rectangle corners in standard order for painting
+		int32_t borderVals[4] = {min_x, max_x, min_y, max_y};
+		for(uint8_t i = 0; i < 4; i++){ // i = iterator for output paintCorner
+			// iterate over the painterCornerss --> rectangle corners in standard order
+			for(uint8_t j = 0; j < 4; j++){ // iterate over the borderValues in standard order
+				// iterator over the 4 border values --> check if the corresponding corner's coordinate matches
+				// j < borders::MIN_Y == checking x's
+				// j > borders::MAX_X == checking y's
+				for(uint8_t k=0; k < 4; k++){ // iterate over all rectangle members: corner[0-3]
+					painterCorners[i] =	((j < borders::MIN_Y) && ( static_cast<int32_t>((corners[k](coordinate::X)*PPM)+0.5) == borderVals[j]))? corners[k] :
+										((j > borders::MAX_X) && ( static_cast<int32_t>((corners[k](coordinate::Y)*PPM)+0.5) == borderVals[j]))? corners[k] : painterCorners[i];
+				}
+			}
+		}
+	}
+
+	// @TODO: Range clipping on the upper bounds
+	for(uint16_t i = (min_x >=0)? min_x: 0; i<max_x; i++){
+		for(uint16_t j = (min_y >=0)? min_y: 0; j<max_y; j++){
+			if(specialCase){
+				painter.newPixel(i,j);
+			} else { // need to do piecewise line checking..
+				Vector2f topCheck[2];
+				Vector2f botCheck[2];
+				topCheck[0] = (i <= static_cast<int32_t>( painterCorners[border::MAX_Y](coordinate::X)*PPM + 0.5 ))? painterCorners[borders::MIN_X] : painterCorners[borders::MAX_Y];
+				topCheck[1] = (i <= static_cast<int32_t>( painterCorners[border::MAX_Y](coordinate::X)*PPM + 0.5 ))? painterCorners[borders::MAX_Y] : painterCorners[borders::MAX_X];
+
+				botCheck[0] = (i <= static_cast<int32_t>( painterCorners[border::MIN_Y](coordinate::X)*PPM + 0.5 ))? painterCorners[borders::MIN_X] : painterCorners[borders::MIN_Y];
+				botCheck[1] = (i <= static_cast<int32_t>( painterCorners[border::MIN_Y](coordinate::X)*PPM + 0.5 ))? painterCorners[borders::MIN_Y] : painterCorners[borders::MAX_X];
+				// top line checking
+				lineCheck(,,i,j);
+				// bottom line checking
+				lineCheck(,,i,j);
+			}
+		}
+	}
+
+	return;
+}
---
+++ graphics/bodies.hpp
@@ -0,0 +1,173 @@
+#ifndef 	__BODIES_H__
+#define		__BODIES_H__
+// override the '+' operator
+// what does + mean for a vector?
+	// need to add eachs element relative to its accessor
+#include <vector>
+#include <stdint.h>
+#include <cmath> // math.h ?
+#include <iostream>
+
+#include <limits> // std::numeric_limits for data types
+
+#include <Eigen/Dense> // eigen file path
+
+using namespace Eigen;
+
+
+// const --> doesnt change but can be evaluated at run time
+// constexpr --> doesnt change but IS EVALUATED AT COMPILE TIME
+
+extern const uint16_t PPM; // -------------- VERY IMPORTANT
+extern const float MPS; // meters per second velocity
+extern const double pi; // important constant
+extern const float dt;
+
+
+// sadly, can't use enum class because it creates its own type.. and eigen expects integer arguments
+enum coordinate {X,Y,Z}; // enum class rip
+
+enum borders {MIN_X,MAX_X,MIN_Y,MAX_Y}; // used for painter object in rectangle
+
+// quantization of real world vectors
+class Pixel { // quantization
+public: // no need for initializer (excluded from initializer lists)
+	std::vector<uint16_t> x;
+	std::vector<uint16_t> y;
+	void newPixel(uint16_t _x, uint16_t _y);
+	void clear();
+	Pixel copy();
+};
+
+// stores both real world vectors and quantized position only vector
+class Ball {
+public:
+	Vector2f pos;
+	Vector2f vel;
+	Vector2f acc;
+
+	float theta,thetaDot,theta2Dot;
+
+	Pixel painter;
+	float radius;
+	int32_t min_x, min_y, max_x, max_y;
+	// all in meters, 16 pixels per meter
+	Ball(float _radius, float _xCenter, float _yCenter);
+	void move(bool angular = false);
+	// add this next
+	// void move(Vector2f jerk);
+	void bounce(uint16_t _width, uint16_t _height);
+	void update();
+	Ball copy();
+	void capVector(uint16_t _width, uint16_t _height);
+};
+
+/*
+	coordinate systems, frames of reference
+
+	want to CHANGE THE BASIS (unit) VECTORS from one frame of reference to another
+
+	vector = n1 * v1 + n2 * v2 + n3 * v3 --> v1-3 are the unit vectors
+
+	u1 = a11 * v1 + a12 * v2 + a13 * v3
+	u2 = a21 * v1 + a22 * v2 + a23 * v3
+	u3 = a31 * v1 + a32 * v2 + a33 * v3
+
+	3x3 'change of basis' matrix: M = [a11, a12, a13], [a21, a22, a23], [a31, a32, a33]
+
+	2 representations of a given vector v:
+		v = aT(v1,v2,v3) and v = bT(u1,u2,u3)
+		
+		a = (n1,n2,n3)T and b = (m1,m2,m3) .. the scalar quantities that correspond to each unit vector
+
+	implication:
+		a = MTrans * b
+		b = inverse(MTrans) * a
+
+	Matrix M could be used to ROTATE and SCALE vectors --> CAN'T DEAL WITH POINTS in graphics
+			we want to TRANSLATE points (and objects)
+
+	arbitrary transformation can be achieved by:
+		mul by 3x3 matrix
+		shift by a vector
+	IN GRAPHICS WE PREFER:
+		use FRAMES to achieve same thing
+
+	Frame is a richer coordinate system
+		reference point P0
+		3 linearly independent basis vectors (v1,v2,v3)
+		represent vectors v and points P differently:
+			v = n1 * v1 + n2 * v2 + n3 * v3
+			P = P0 + v (!!!!)
+
+	v = [n1, n2, n3, 0]*[v1,v2,v3,P0]T
+	P = [n1, n2, n3, 1]*[v1,v2,v3,P0]T
+
+	coefficients ^^^ --> homogeneous coordinates of v and P respectively
+
+	change of frames
+
+	from frame (v1,v2,v3,P0) to new frame (u1,u2,u3,Q0)
+		u1 = a11*v11 + a12*v2 + a13*v3
+		u2 = ..
+		u3 = ..
+		Q0 = a41*v1 + a42*v2 + a43*v3 + P0
+
+	4x4 matrix:
+		M = [[a11,a12,a13,0],[a21,a22,a23,0],[a31,a32,a33,0],[a41,a42,a43,1]]
+
+	a and b are homogeneous representations of same point/vector with respect to 2 frames:
+		a = [v1,v2,v3,P0],b=[u1,u2,u3,Q0] = btrans * M [v1,v2,v3,P0]
+
+	implies:
+		a = Mtrans * b
+		b = inverse(MTrans) * a
+
+	Affine transformation
+		transposed matrix:
+			MTrans = 	|a11,	a21,	a31,	a41 |
+							|a12,	a22,	a32,	a42 |
+							|a13,	a23,	a33,	a43 |
+							|0,		0,		0,		1	|
+		-- an arbitrary AFFINE TRANSFORMATION
+			12 degrees of freedom
+				9 DOF as the 3x3 matrix
+				+ components of a VECTOR SHIFT
+
+	most important affine transformations:
+		rotation
+		scaling
+		translation
+	all affine transformations can be expressed as a combination of all 3 of those
+*/
+
+
+// going to use Eigen library for my linear algebra from here on!
+
+
+class Rectangle {
+public:
+	float length; // magnitude vectors
+	float width; // magnitude vectors
+
+	Vector2f pos;
+	Vector2f vel;
+	Vector2f acc;
+
+	Vector2f corners[4];
+
+	// attitude: angle, angular velocity, angular acceleration
+	float theta, thetaDot, theta2Dot;
+
+	// shape vectors
+	// PVector lVec;
+	// PVector wVec;
+
+	Pixel painter;
+	int32_t min_x,max_x,min_y,max_y;
+	// for now, rotational frame is not configurable by developer
+	Rectangle(float _length, float _width, float _posX, float _posY);
+	void update();
+};
+
+#endif	//__BODIES_H__
